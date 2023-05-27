ad100719800b4d778a1d8e4a1b371369b7635ade
Lola <73179696+Lola0810@users.noreply.github.com>
2023-05-27 17:00:17 +0200

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,19 @@
+# PROJET DE SAE 204 - Portfolio
+## Rendu final
+
+### Ce que j'ai abandonné par rapport à la maquette :
+- Pouvoir changer la langue du site internet
+    -> Pour l'instant mon objectif n'est pas de m'internationnalisé, je souhaite déjà toucher un public français.
+- Le formulaire
+    -> J'ai utilisé l'API de Nathanaël Pean car c'était une bonne initative de sa part
+
+### Les problèmes que je n'ai pas reglé :
+- Le problème de contrast constaté par exemple ma photo sur la page d'accueil
+    -> Je n'arrive pas à régler le problème du reflet qui cause en partie ce problème de contrast
+
+### Au niveau du contenu : 
+- Je n'ai pas implémenté le contenu de mes vrais projets car j'attends d'avoir une base de donnée de projets plus professionnels pour pouvoir les mettre sur mon Portfolio mais le fichier projects.js est déjà prêt à accueilir le contenu de mes projets.
+- Sinon tous les textes sont les textes finaux qui ne seront pas modifiés.
+
+### Au niveau du design :
+J'ai essayé de faire un design épuré, professionnel et me représentant.
---
+++ src/js/projects.js
@@ -0,0 +1,85 @@
+const PROJECTS = [
+    {   
+        title: "Projet test",
+        description: "Description rapide",
+        thumbnail: "image.jpg",
+        duration: {
+            start: "10-20-2023",
+            end: "10-21-2024"
+        },
+        baseline: "Projet mmi",
+        assignments: [
+            "Lorem ipsum dolor sit amet ..",
+            "Lorem ipsum dolor sit amet"
+        ],
+        technologies: [
+            "Logiciels: ", 
+            "langages de programmation: ",
+            "Outils de gestion de projet: "
+        ],
+        note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus nunc tellus, ac maximus nisl convallis eget. Donec rutrum nunc vel pharetra pellentesque. Pellentesque varius consequat neque, nec malesuada justo vestibulum a. Maecenas nec nisl non est feugiat aliquet sit amet sit amet mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin condimentum lectus, ac bibendum dolor finibus a. Morbi fermentum urna non est varius efficitur. Fusce euismod nisi eget libero ultricies, in pellentesque odio bibendum."
+    }
+];
+
+const DESIGNS = [
+    {
+        title: "Projet test2",
+        description: "Description rapide",
+        thumbnail: "image.jpg",
+        duration: {
+            start: "10-20-2023",
+            end: "10-21-2024"
+        },
+        baseline: "Projet personnel",
+        assignments: [
+            "Lorem ipsum dolor sit amet ..",
+            "Lorem ipsum dolor sit amet"
+        ],
+        technologies: [
+            "Logiciels: ", 
+            "langages de programmation: ",
+            "Outils de gestion de projet: "
+        ],
+        note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus nunc tellus, ac maximus nisl convallis eget. Donec rutrum nunc vel pharetra pellentesque. Pellentesque varius consequat neque, nec malesuada justo vestibulum a. Maecenas nec nisl non est feugiat aliquet sit amet sit amet mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin condimentum lectus, ac bibendum dolor finibus a. Morbi fermentum urna non est varius efficitur. Fusce euismod nisi eget libero ultricies, in pellentesque odio bibendum."
+    },
+    {
+        title: "Projet test3",
+        description: "Description rapide",
+        thumbnail: "image.jpg",
+        duration: {
+            start: "10-20-2023",
+            end: "10-21-2024"
+        },
+        baseline: "Projet personnel",
+        assignments: [
+            "Lorem ipsum dolor sit amet ..",
+            "Lorem ipsum dolor sit amet"
+        ],
+        technologies: [
+            "Logiciels: ", 
+            "langages de programmation: ",
+            "Outils de gestion de projet: "
+        ],
+        note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus nunc tellus, ac maximus nisl convallis eget. Donec rutrum nunc vel pharetra pellentesque. Pellentesque varius consequat neque, nec malesuada justo vestibulum a. Maecenas nec nisl non est feugiat aliquet sit amet sit amet mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin condimentum lectus, ac bibendum dolor finibus a. Morbi fermentum urna non est varius efficitur. Fusce euismod nisi eget libero ultricies, in pellentesque odio bibendum."
+    },
+    {
+        title: "Projet test4",
+        description: "Description rapide",
+        thumbnail: "image.jpg",
+        duration: {
+            start: "10-20-2023",
+            end: "10-21-2024"
+        },
+        baseline: "Projet personnel",
+        assignments: [
+            "Lorem ipsum dolor sit amet ..",
+            "Lorem ipsum dolor sit amet"
+        ],
+        technologies: [
+            "Logiciels: ", 
+            "langages de programmation: ",
+            "Outils de gestion de projet: "
+        ],
+        note: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus nunc tellus, ac maximus nisl convallis eget. Donec rutrum nunc vel pharetra pellentesque. Pellentesque varius consequat neque, nec malesuada justo vestibulum a. Maecenas nec nisl non est feugiat aliquet sit amet sit amet mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin condimentum lectus, ac bibendum dolor finibus a. Morbi fermentum urna non est varius efficitur. Fusce euismod nisi eget libero ultricies, in pellentesque odio bibendum."
+    }
+];
